9fa41c2a1c7b99dc6fa8e278d5092961a782e419
22025566 <22025566@myrp.edu.sg>
2023-05-04 11:25:59 +0800

    completed up till section C

--- app/src/main/java/sg/edu/rp/c346/id22025566/week03/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22025566/week03/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.c346.id22025566.week03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,13 +16,26 @@ public class MainActivity extends AppCompatActivity {
     EditText etInput;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //all codes here will be executed
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         tvDisplay = findViewById(R.id.textViewDisplay);
         btnDisplay = findViewById(R.id.buttonDisplay);
         etInput = findViewById(R.id.editTextInput);
+
+        btnDisplay.setOnClickListener(new View.OnClickListener() { //must be inside onCreate()
+            @Override
+            public void onClick(View v) { // onClick method
+                // Code for the action
+                String stringResponse = etInput.getText().toString();
+                tvDisplay.setText(stringResponse);
+
+
+
+            }
+        });
+
 
     }
 }
